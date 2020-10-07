a0d5e079be126aa2a877274be2f6c5b6eadc31c3
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-10-07 18:25:17 +0530

    Fixed ranklist

--- ranklist/get_list.py
+++ ranklist/get_list.py
@@ -2,6 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+quests = ['Explore Machine Learning Models with Explainable AI',
+          'Integrate with Machine Learning APIs',
+          'Engineer Data in Google Cloud',
+          'Insights from Data with BigQuery',
+          'Perform Foundational Data, ML, and AI Tasks in Google Cloud',
+          'Deploy to Kubernetes in Google Cloud',
+          'Build and Secure Networks in Google Cloud',
+          'Deploy and Manage Cloud Environments with Google Cloud',
+          'Set up and Configure a Cloud Environment in Google Cloud',
+          'Perform Foundational Infrastructure Tasks in Google Cloud',
+          'Getting Started: Create and Manage Cloud Resources',
+]
+
+
+
 def getDetailsForProfile(url):
 
     try:
@@ -13,17 +28,24 @@ def getDetailsForProfile(url):
 
     name_h1 = soup.find("h1", {"class": "l-mbm"})
     labs_quests_p = soup.find("p", {"class": "public-profile__hero__details"})
+    badges_divs = soup.find_all("div", {"class": "public-profile__badge"})
+
 
     if name_h1 is None:
         return -1, -1
+    c=0
+    for badge in badges_divs:
+        b_arr = badge.text.strip().rsplit("\n")
+        if b_arr[0] in quests:
+            c+=1
 
 
     labs_quests = labs_quests_p.text.strip().rsplit("\n")
 
-    return int(labs_quests[0]), int(labs_quests[3])
+    return int(labs_quests[0]), c
 
 
-def execute(filename):
+def execute(filename='test_urls.csv'):
 
     print(filename)
     try:
@@ -42,7 +64,6 @@ def execute(filename):
     print()
     data['labs']=labs
     data['quests']=quests
-    print(data)
     return data
 
 if __name__ == '__main__':
